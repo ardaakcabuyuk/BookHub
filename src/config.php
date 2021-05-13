56d7bc30118dc0f37ee4192fda47a4c521f3550a
Adil Meric <adilmeric13@gmail.com>
2021-05-13 16:57:30 +0300

    edition info

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 if(!defined('host')) define('host', 'localhost');
 if(!defined('dbname')) define('dbname', 'book_hub');
 if(!defined('username')) define('username', 'root');
-if(!defined('passwd')) define('passwd', 'root');
+if(!defined('passwd')) define('passwd', '');
 $db = mysqli_connect(host, username, passwd, dbname);
 if($db === false) {
   die("Failed to connect MYSQL: " . mysqli_connect_errno());
